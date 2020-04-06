5ae6f8fd24bbf6b0a6d05b51a14ce7e5166b8eac
조경은 <ruddms0127@naver.com>
2020-04-06 14:29:47 +0900

    Database 완성
    Former-commit-id: d7e64711cb3875e4a8e2ac2fd9d71167e6d8c0c0

--- sub2/backend/api/models.py
+++ sub2/backend/api/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 
 class Store(models.Model):
-    id = models.IntegerField(primary_key=True)
+    store_id = models.IntegerField(primary_key=True)
     store_name = models.CharField(max_length=50)
     branch = models.CharField(max_length=20, null=True)
     area = models.CharField(max_length=50, null=True)
@@ -11,8 +11,70 @@ class Store(models.Model):
     address = models.CharField(max_length=200, null=True)
     latitude = models.FloatField(max_length=10, null=True)
     longitude = models.FloatField(max_length=10, null=True)
+
+    def __str__(self):
+        return self.store_name
+
+
+class User(models.Model):
+    user_id = models.AutoField(primary_key=True)
+    email = models.CharField(max_length=500, null=True)
+    token = models.CharField(max_length=500, blank=True)
+    name = models.CharField(max_length=50, null=True)
+    password = models.CharField(max_length=100, null=True)
+    gender = models.IntegerField()
+    born_year = models.CharField(max_length=50)
+    profileimg = models.CharField(max_length=500, blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Category(models.Model):
+    category_id = models.AutoField(primary_key=True)
+    store_id = models.ForeignKey('Store', on_delete=models.CASCADE, null=True)
     category = models.CharField(max_length=200, null=True)
 
-    @property
-    def category_list(self):
-        return self.category.split("|") if self.category else []
+    def __str__(self):
+        return self.category
+
+
+class Bhour(models.Model):
+    store_id = models.ForeignKey('Store', on_delete=models.CASCADE)
+    type = models.IntegerField()
+    week_type = models.IntegerField()
+    mon = models.IntegerField()
+    tue = models.IntegerField()
+    wed = models.IntegerField()
+    thu = models.IntegerField()
+    fri = models.IntegerField()
+    sat = models.IntegerField()
+    sun = models.IntegerField()
+    start_time = models.DateTimeField()
+    end_time = models.DateTimeField()
+    etc = models.TextField(max_length=100, blank=True)
+
+    def __str__(self):
+        return self.store_id
+
+
+class Menu(models.Model):
+    menu_id = models.AutoField(primary_key=True)
+    store_id = models.ForeignKey('Store', on_delete=models.CASCADE)
+    menu = models.CharField(max_length=200, null=True)
+    price = models.IntegerField()
+
+    def __str__(self):
+        return self.menu_id
+
+
+class Review(models.Model):
+    review_id = models.AutoField(primary_key=True)
+    store_id = models.ForeignKey('Store', on_delete=models.CASCADE)
+    user_id = models.ForeignKey('User', on_delete=models.CASCADE)
+    total_score = models.IntegerField(null=True)
+    content = models.TextField(blank=True)
+    reg_time = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.review_id

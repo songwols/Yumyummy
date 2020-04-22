e49577bc5cbe68e927d9c1057ac29d7d3891850c
chokyungeun <ruddms0127@naver.com>
2020-04-22 09:17:51 +0900

    임시저장
    Former-commit-id: 21242c7b4de567ac10dddc18b4b6cdadc27d131b

--- sub2/frontend/src/components/Paging/index.jsx
+++ sub2/frontend/src/components/Paging/index.jsx
@@ -1,13 +1,12 @@
 import React from "react";
 import styled from "styled-components";
+import { Link } from "react-router-dom";
 
 class Paging extends React.Component {
   render() {
     return (
       <div>
-        <Link to={"/"} style={{ textDecoration: "none" }}>
-          <Button></Button>
-        </Link>
+        <Button onClick={}></Button>
       </div>
     );
   }

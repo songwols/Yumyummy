85604a0c373301993740cae3484570e4e06b7fe4
chokyungeun <ruddms0127@naver.com>
2020-04-23 13:33:38 +0900

    menu띄우기
    Former-commit-id: 284dd3dac22737d76f8031a515a495e6cffaa5fe

--- sub2/frontend/src/stores/index.js
+++ sub2/frontend/src/stores/index.js
@@ -1,6 +1,7 @@
 import TestStore from "./testStore";
 import StoreStore from "./storeStore";
 import ReviewStore from "./reviewStore";
+import MenuStore from "./menuStore";
 import SuggestStore from "./suggestStore";
 
 class RootStore {
@@ -8,6 +9,7 @@ class RootStore {
     this.testStore = new TestStore(this);
     this.storeStore = new StoreStore(this);
     this.reviewStore = new ReviewStore(this);
+    this.menuStore = new MenuStore(this);
     this.suggestStore = new SuggestStore(this);
   }
 }

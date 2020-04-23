32fb3dcf7782c815db8393dc9f32c8085227921b
woojae <woojae121@gmail.com>
2020-04-23 13:40:45 +0900

    port 8000 -> port 8080
    Former-commit-id: b53c7e4a66ef228680c5ca69be1cba60e8fee633

--- sub2/frontend/src/agent.js
+++ sub2/frontend/src/agent.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 // const API_ROOT = "http://52.79.156.160/api";
-const API_ROOT = "http://localhost:8000/api";
+const API_ROOT = "http://localhost:8080/api";
 
 const responseBody = (res) => res.body;
 
